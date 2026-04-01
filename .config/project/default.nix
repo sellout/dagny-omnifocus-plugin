ecacbc94a15a869ef538be190abc8a697df567f1
Greg Pfeil <greg@technomadic.org>
2026-04-01 14:46:12 -0600

    Initial setup

---
+++ .config/project/default.nix
@@ -0,0 +1,11 @@
+### All available options for this file are listed in
+### https://sellout.github.io/project-manager/options.xhtml
+{...}: {
+  project = {
+    name = "dagny-omnifocus-plugin";
+    summary = "Dagny / OmniFocus integration";
+  };
+
+  ## publishing
+  services.github.settings.repository.topics = [];
+}
